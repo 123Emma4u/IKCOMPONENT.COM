493f03f8d01950e64bab3bbf1cc79cc5d4755157
123Emma4u <168229547+123Emma4u@users.noreply.github.com>
2024-07-01 00:25:21 +0100

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb+srv://chinonsoemmanuel221:<password>@cluster1.mjuceut.mongodb.net/", {
+  mongoose.connect("mongodb+srv://chinonsoemmanuel221:ChiEmmanonnuelso@cluster1.mjuceut.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
